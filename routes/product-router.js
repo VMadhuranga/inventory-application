cc002bfbe6a4a8350027329d1de591aab71d98b8
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-01-16 12:02:44 +0530

    Create routers to handle product create GET and POST requests

--- routes/product-router.js
+++ routes/product-router.js
@@ -5,6 +5,8 @@ const productController = require("../controllers/product-controller");
 const router = express.Router();
 
 router.get("/products", productController.productList);
+router.get("/product/create", productController.productCreateGET);
+router.post("/product/create", productController.productCreatePOST);
 router.get("/product/:id", productController.productDetail);
 
 module.exports = router;
