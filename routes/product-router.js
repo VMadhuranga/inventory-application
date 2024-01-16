f3da50852fc6e4bdb1b68e87b16d540598607116
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-01-16 14:31:00 +0530

    Create routers to handle product update GET and POST requests

--- routes/product-router.js
+++ routes/product-router.js
@@ -7,6 +7,8 @@ const router = express.Router();
 router.get("/products", productController.productList);
 router.get("/product/create", productController.productCreateGET);
 router.post("/product/create", productController.productCreatePOST);
+router.get("/product/:id/update", productController.productUpdateGET);
+router.post("/product/:id/update", productController.productUpdatePOST);
 router.get("/product/:id", productController.productDetail);
 
 module.exports = router;
