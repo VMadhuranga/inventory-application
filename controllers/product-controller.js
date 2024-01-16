6757c79a15cedf0aa81fbb14f9ae89a29a36e6a8
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-01-16 12:02:15 +0530

    Create productCreateGET and productCreatePost controllers

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,6 +1,10 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 const ProductModel = require("../models/product-model");
+const ManufacturerModel = require("../models/manufacturer-model");
+const CategoryModel = require("../models/category-model");
+const SizeModel = require("../models/size-model");
 
 const productList = asyncHandler(async (req, res, next) => {
   const allProducts = await ProductModel.find({}, "name")
@@ -31,7 +35,140 @@ const productDetail = asyncHandler(async (req, res, next) => {
   });
 });
 
+const productCreateGET = asyncHandler(async (req, res, next) => {
+  const [allManufacturers, allCategories, allSizes] = await Promise.all([
+    ManufacturerModel.find({}).exec(),
+    CategoryModel.find({}).exec(),
+    SizeModel.find({}).exec(),
+  ]);
+
+  res.render("product-form-view", {
+    title: "Create Product",
+    manufacturers: allManufacturers,
+    categories: allCategories,
+    sizes: allSizes,
+  });
+});
+
+const productCreatePOST = [
+  (req, res, next) => {
+    if (!Array.isArray(req.body["product-categories"])) {
+      req.body["product-categories"] =
+        typeof req.body["product-categories"] === "undefined"
+          ? []
+          : [req.body["product-categories"]];
+    }
+
+    next();
+  },
+
+  (req, res, next) => {
+    if (!Array.isArray(req.body["product-sizes"])) {
+      req.body["product-sizes"] =
+        typeof req.body["product-sizes"] === "undefined"
+          ? []
+          : [req.body["product-sizes"]];
+    }
+
+    next();
+  },
+
+  (req, res, next) => {
+    if (!Array.isArray(req.body["product-size-quantity"])) {
+      req.body["product-size-quantity"] =
+        typeof req.body["product-size-quantity"] === "undefined"
+          ? []
+          : [req.body["product-size-quantity"]];
+    }
+
+    next();
+  },
+
+  body("product-name", "Product name must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("product-description", "Product description must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("product-manufacturer", "Product manufacturer must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("product-categories", "Select at least one category")
+    .isArray({ min: 1 })
+    .escape(),
+  body("product-sizes", "Select at least one size")
+    .isArray({ min: 1 })
+    .escape(),
+  body("product-size-quantity", "Product quantity must not be empty")
+    .isArray({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const newProduct = new ProductModel({
+      name: req.body["product-name"],
+      description: req.body["product-description"],
+      manufacturer: req.body["product-manufacturer"],
+      categories: req.body["product-categories"],
+    });
+
+    const productSizes = req.body["product-sizes"];
+    const productSizeQuantity = req.body["product-size-quantity"];
+
+    productSizes.forEach((size, index) => {
+      newProduct.availableSizes.push({
+        size: size,
+        quantity: productSizeQuantity[index],
+      });
+    });
+
+    if (!errors.isEmpty()) {
+      const [allManufacturers, allCategories, allSizes] = await Promise.all([
+        ManufacturerModel.find({}).exec(),
+        CategoryModel.find({}).exec(),
+        SizeModel.find({}).exec(),
+      ]);
+
+      allCategories.forEach((category) => {
+        if (newProduct.categories.includes(category._id)) {
+          category.checked = true;
+        }
+      });
+
+      allSizes.forEach((size) => {
+        if (
+          newProduct.availableSizes.find(({ size: selectedSize }) =>
+            selectedSize.toString().includes(size._id),
+          )
+        ) {
+          size.checked = true;
+        }
+      });
+
+      res.render("product-form-view", {
+        title: "Create Product",
+        manufacturers: allManufacturers,
+        categories: allCategories,
+        sizes: allSizes,
+        product: newProduct,
+        errors: errors.array(),
+      });
+
+      return;
+    } else {
+      await newProduct.save();
+      res.redirect(newProduct.url);
+    }
+  }),
+];
+
 module.exports = {
   productList,
   productDetail,
+  productCreateGET,
+  productCreatePOST,
 };
