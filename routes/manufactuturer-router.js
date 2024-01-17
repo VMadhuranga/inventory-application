7da7fb924dde455385cd0cdf7f4810a0e2aca590
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-01-17 13:22:59 +0530

    Create routers to handle manufacturer create GET and POST requests

--- routes/manufactuturer-router.js
+++ routes/manufactuturer-router.js
@@ -5,6 +5,11 @@ const manufactureController = require("../controllers/manufacturer-controller");
 const router = express.Router();
 
 router.get("/manufacturers", manufactureController.manufacturerListGET);
+router.get("/manufacturer/create", manufactureController.manufacturerCreateGET);
+router.post(
+  "/manufacturer/create",
+  manufactureController.manufacturerCreatePOST,
+);
 router.get("/manufacturer/:id", manufactureController.manufacturerDetailGET);
 
 module.exports = router;
