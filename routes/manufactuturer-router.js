e9a8e4a9af241cbb0e1049bf7f0fc68839440914
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-01-17 11:48:12 +0530

    Create router to handle manufacturer detail GET request

--- routes/manufactuturer-router.js
+++ routes/manufactuturer-router.js
@@ -5,5 +5,6 @@ const manufactureController = require("../controllers/manufacturer-controller");
 const router = express.Router();
 
 router.get("/manufacturers", manufactureController.manufacturerListGET);
+router.get("/manufacturer/:id", manufactureController.manufacturerDetailGET);
 
 module.exports = router;
