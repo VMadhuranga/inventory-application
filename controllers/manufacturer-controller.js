f3138fd297dde96c63b220a0ba520cfad2e3a0c2
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-01-17 11:44:28 +0530

    Create manufacturerDetailGET controller

--- controllers/manufacturer-controller.js
+++ controllers/manufacturer-controller.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 
 const ManufacturerModel = require("../models/manufacturer-model");
+const ProductModel = require("../models/product-model");
 
 const manufacturerListGET = asyncHandler(async (req, res, next) => {
   const allManufacturers = await ManufacturerModel.find({}, "name")
@@ -13,6 +14,27 @@ const manufacturerListGET = asyncHandler(async (req, res, next) => {
   });
 });
 
+const manufacturerDetailGET = asyncHandler(async (req, res, next) => {
+  const [manufacturer, manufacturerProducts] = await Promise.all([
+    ManufacturerModel.findById(req.params.id).exec(),
+    ProductModel.find({ manufacturer: req.params.id }, "name").exec(),
+  ]);
+
+  if (manufacturer === null) {
+    const error = new Error("Manufacturer not found");
+    error.status = 404;
+
+    return next(error);
+  }
+
+  res.render("manufacturer-detail-view", {
+    title: manufacturer.name,
+    manufacturer: manufacturer,
+    manufacturerProducts: manufacturerProducts,
+  });
+});
+
 module.exports = {
   manufacturerListGET,
+  manufacturerDetailGET,
 };
