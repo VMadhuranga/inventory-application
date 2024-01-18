8ba2a5974f745aacdf00a3bc3393ab8fd7e6fe34
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-01-18 14:08:27 +0530

    Create router to handle product size detail GET request

--- routes/prod-size-router.js
+++ routes/prod-size-router.js
@@ -5,5 +5,6 @@ const prodSizeController = require("../controllers/prod-size-controller");
 const router = express.Router();
 
 router.get("/sizes", prodSizeController.prodSizeListGET);
+router.get("/size/:id", prodSizeController.prodSizeDetailGET);
 
 module.exports = router;
