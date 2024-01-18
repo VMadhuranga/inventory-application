403f28bccc3d37a59a9e36f8ee7ec07da3f8e7ae
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-01-18 11:04:51 +0530

    Create routers to handle category create GET and POST requests

--- routes/category-router.js
+++ routes/category-router.js
@@ -5,6 +5,8 @@ const categoryController = require("../controllers/category-controller");
 const router = express.Router();
 
 router.get("/categories", categoryController.categoryListGET);
+router.get("/category/create", categoryController.categoryCreateGET);
+router.post("/category/create", categoryController.categoryCreatePOST);
 router.get("/category/:id", categoryController.categoryDetailGET);
 
 module.exports = router;
