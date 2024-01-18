de3c6fa0d6a4dd52e7af069b8617e81adc9a4489
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-01-18 09:43:27 +0530

    Create router to handle category detail GET request

--- routes/category-router.js
+++ routes/category-router.js
@@ -5,5 +5,6 @@ const categoryController = require("../controllers/category-controller");
 const router = express.Router();
 
 router.get("/categories", categoryController.categoryListGET);
+router.get("/category/:id", categoryController.categoryDetailGET);
 
 module.exports = router;
