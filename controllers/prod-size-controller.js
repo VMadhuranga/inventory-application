a5a3fe9c14c06914ca0a1bd1c620847a6669b019
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-01-18 14:07:52 +0530

    Create prodSizeDetailGET controller

--- controllers/prod-size-controller.js
+++ controllers/prod-size-controller.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 
 const SizeModel = require("../models/size-model");
+const ProductModel = require("../models/product-model");
 
 const prodSizeListGET = asyncHandler(async (req, res, next) => {
   const allSizes = await SizeModel.find({}).sort({ type: 1 }).exec();
@@ -11,6 +12,27 @@ const prodSizeListGET = asyncHandler(async (req, res, next) => {
   });
 });
 
+const prodSizeDetailGET = asyncHandler(async (req, res, next) => {
+  const [size, sizeProducts] = await Promise.all([
+    SizeModel.findById(req.params.id).exec(),
+    ProductModel.find({ "availableSizes.size": req.params.id }).exec(),
+  ]);
+
+  if (size === null) {
+    const error = new Error("Size not found");
+    error.status = 404;
+
+    return next(error);
+  }
+
+  res.render("size-detail-view", {
+    title: size.type,
+    size: size,
+    sizeProducts: sizeProducts,
+  });
+});
+
 module.exports = {
   prodSizeListGET,
+  prodSizeDetailGET,
 };
