39b1b9a67a2ddbb813139a4675cc193ad978cff2
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-01-18 09:42:47 +0530

    Create categoryDetailGET controller

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 
 const CategoryModel = require("../models/category-model");
+const ProductModel = require("../models/product-model");
 
 const categoryListGET = asyncHandler(async (req, res, next) => {
   const allCategories = await CategoryModel.find({}, "name")
@@ -13,6 +14,27 @@ const categoryListGET = asyncHandler(async (req, res, next) => {
   });
 });
 
+const categoryDetailGET = asyncHandler(async (req, res, next) => {
+  const [category, categoryProducts] = await Promise.all([
+    CategoryModel.findById(req.params.id).exec(),
+    ProductModel.find({ categories: req.params.id }, "name").exec(),
+  ]);
+
+  if (category === null) {
+    const error = new Error("Category not found");
+    error.status = 404;
+
+    return next(error);
+  }
+
+  res.render("category-detail-view", {
+    title: category.name,
+    category: category,
+    categoryProducts: categoryProducts,
+  });
+});
+
 module.exports = {
   categoryListGET,
+  categoryDetailGET,
 };
