9bf40cfca793bf88a60f8687e700936eff5a6f95
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-01-20 09:23:58 +0530

    Create routers to handle product size update GET and POST requests

--- routes/prod-size-router.js
+++ routes/prod-size-router.js
@@ -7,6 +7,8 @@ const router = express.Router();
 router.get("/sizes", prodSizeController.prodSizeListGET);
 router.get("/size/create", prodSizeController.prodSizeCreateGET);
 router.post("/size/create", prodSizeController.prodSizeCreatePOST);
+router.get("/size/:id/update", prodSizeController.prodSizeUpdateGET);
+router.post("/size/:id/update", prodSizeController.prodSizeUpdatePOST);
 router.get("/size/:id", prodSizeController.prodSizeDetailGET);
 
 module.exports = router;
