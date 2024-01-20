c77c39e817a49ea781c40451b3b1095d63aaa8db
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-01-20 09:51:37 +0530

    Create routers to handle product size delete GET and POST requests

--- routes/prod-size-router.js
+++ routes/prod-size-router.js
@@ -9,6 +9,8 @@ router.get("/size/create", prodSizeController.prodSizeCreateGET);
 router.post("/size/create", prodSizeController.prodSizeCreatePOST);
 router.get("/size/:id/update", prodSizeController.prodSizeUpdateGET);
 router.post("/size/:id/update", prodSizeController.prodSizeUpdatePOST);
+router.get("/size/:id/delete", prodSizeController.prodSizeDeleteGET);
+router.post("/size/:id/delete", prodSizeController.prodSizeDeletePOST);
 router.get("/size/:id", prodSizeController.prodSizeDetailGET);
 
 module.exports = router;
