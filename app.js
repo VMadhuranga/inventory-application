21b636eb73456c3d5e1c3384917c33f9c065f195
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-01-21 13:36:06 +0530

    Setup deploy configurations

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const compression = require("compression");
+const helmet = require("helmet");
+const RateLimit = require("express-rate-limit");
 
 const indexRouter = require("./routes/index");
 const productRouter = require("./routes/product-router");
@@ -12,6 +15,14 @@ const categoryRouter = require("./routes/category-router");
 const prodSizeRouter = require("./routes/prod-size-router");
 
 const app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+const limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
@@ -32,6 +43,10 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression());
+app.use(helmet());
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
